f6250dbb32a50e56a065a8609d76cc22db9f8e86
Cecilia Sanare <ceci@sanare.dev>
2024-02-22 19:54:21 -0600

    feat: added metadata

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -10,7 +10,6 @@ const credentialTypes = {
 
 export const options: NextAuthConfig = {
   secret: process.env.AUTH_SECRET,
-  debug: process.env.NODE_ENV === 'development',
   providers: [
     CredentialsProvider({
       name: 'Email',
